6778fd13c99e0bf3f5744e2c3214dbcf37d94cb4
masterPsy <15216134134@163.com>
2026-07-03 11:06:35 +0800

    init

--- module-common/src/main/java/com/common/annotation/Auth.java
+++
@@ -1,9 +0,0 @@
-package com.common.annotation;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.METHOD,ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface Auth {
-}
